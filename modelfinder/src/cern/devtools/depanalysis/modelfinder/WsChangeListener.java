acbdffb43d41ab1b67cd33c8e113ce88fa850437
Donat Csikos <csdonat@gmail.com>
2012-05-25 17:44:21 +0200

    changed copyright

--- modelfinder/src/cern/devtools/depanalysis/modelfinder/WsChangeListener.java
+++ modelfinder/src/cern/devtools/depanalysis/modelfinder/WsChangeListener.java
@@ -1,8 +1,6 @@
 /*
  * File WsChangeListener.java.
  * Created by Donat Csikos<dcsikos@cern.ch> at 25 May 2012.
- *
- * Copyright CERN 2012, All Rights Reserved.
  */
 package cern.devtools.depanalysis.modelfinder;
 
